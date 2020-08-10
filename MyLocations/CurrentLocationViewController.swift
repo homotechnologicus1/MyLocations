bc7afce422660b33ef9e91032fbae18f8297b568
joe <homo.technologicus1@gmail.com>
2020-08-11 02:24:48 +0900

    done with 'Date formatting' (in displaying location info)

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -36,6 +36,16 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
         updateLabels()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+    
     
     // MARK:- Helper Methods
     func showLocationServicesDeniedAlert() {
@@ -198,7 +208,14 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
         }
     }
 
-    
+    // MARK:- Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "TagLocation" {
+            let controller = segue.destination as! LocationDetailsViewController
+            controller.coordinate = location!.coordinate
+            controller.placemark = placemark
+        }
+    }
     
     // MARK: - CLLocationManagerDelegate
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
---
+++ MyLocations/LocationDetailsViewController.swift
@@ -0,0 +1,93 @@
+//
+//  LocationDetailsViewController.swift
+//  MyLocations
+//
+//  Created by joe_mac on 08/10/2020.
+//  Copyright © 2020 Joe K. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+private let dateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .medium
+    formatter.timeStyle = .short
+    return formatter
+}()
+
+class LocationDetailsViewController: UITableViewController {
+    
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    var coordinate = CLLocationCoordinate2D(latitude: 0,
+                                           longitude: 0)
+    var placemark: CLPlacemark?
+
+    
+
+    // MARK:- Actions
+    @IBAction func done() {
+      navigationController?.popViewController(animated: true)
+    }
+
+    @IBAction func cancel() {
+      navigationController?.popViewController(animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        descriptionTextView.text = ""
+        categoryLabel.text = ""
+        
+        latitudeLabel.text = String(format: "%.8f",
+                                    coordinate.latitude)
+        longitudeLabel.text = String(format: "%.8f",
+                                     coordinate.longitude)
+        
+        if let placemark = placemark {
+            addressLabel.text = string(from: placemark)
+        } else {
+            addressLabel.text = "No Address Found"
+        }
+        
+        dateLabel.text = format(date: Date())
+    }
+
+    // MARK:- Helper Methods
+    func string(from placemark: CLPlacemark) -> String {
+        var text = ""
+        
+        if let s = placemark.subThoroughfare {
+            text += s + " "
+        }
+        if let s = placemark.thoroughfare {
+            text += s + ", "
+        }
+        if let s = placemark.locality {
+            text += s + ", "
+        }
+        if let s = placemark.administrativeArea {
+            text += s + " "
+        }
+        if let s = placemark.postalCode {
+            text += s + ", "
+        }
+        if let s = placemark.country {
+            text += s
+        }
+        return text
+    }
+    
+    func format(date: Date) -> String {
+        return dateFormatter.string(from: date)
+    }
+
+
+}
