9962eb95d9f144ee84ef0adf291a64e55b0e6f19
joe <homo.technologicus1@gmail.com>
2020-08-25 00:03:40 +0900

    Table view sections (Table view sections using Core Data done)

--- MyLocations/LocationsViewController.swift
+++ MyLocations/LocationsViewController.swift
@@ -21,16 +21,16 @@ class LocationsViewController: UITableViewController {
             let entity = Location.entity()
             fetchRequest.entity = entity
             
-            let sortDescriptor = NSSortDescriptor(key: "date",
-                                                  ascending: true)
-            fetchRequest.sortDescriptors = [sortDescriptor]
+            let sort1 = NSSortDescriptor(key: "category", ascending: true)
+            let sort2 = NSSortDescriptor(key: "date", ascending: true)
+            fetchRequest.sortDescriptors = [sort1, sort2]
             
             fetchRequest.fetchBatchSize = 20
             
             let fetchedResultsController = NSFetchedResultsController(
                 fetchRequest: fetchRequest,
                 managedObjectContext: self.managedObjectContext,
-                sectionNameKeyPath: nil, cacheName: "Locations")
+                sectionNameKeyPath: "category", cacheName: "Locations")
             
             fetchedResultsController.delegate = self
             return fetchedResultsController
@@ -99,6 +99,16 @@ class LocationsViewController: UITableViewController {
                 fatalCoreDataError(error)
             }
         }
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+            return fetchedResultsController.sections!.count
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            titleForHeaderInSection section: Int) -> String? {
+        let sectionInfo = fetchedResultsController.sections![section]
+        return sectionInfo.name
     }
 
     
