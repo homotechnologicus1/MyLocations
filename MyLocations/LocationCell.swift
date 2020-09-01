9b008545fe385c17a3fc8b355afe58300d9f0b80
joe <homo.technologicus1@gmail.com>
2020-09-01 23:53:41 +0900

    thumbnails via UIImage extension; all of the functionality done

--- MyLocations/LocationCell.swift
+++ MyLocations/LocationCell.swift
@@ -12,7 +12,8 @@ class LocationCell: UITableViewCell {
     
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
-
+    @IBOutlet weak var photoImageView: UIImageView!
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -50,7 +51,15 @@ class LocationCell: UITableViewCell {
                 "Lat: %.8f, Long: %.8f", location.latitude,
                                          location.longitude)
         }
+        
+        photoImageView.image = thumbnail(for: location)
     }
 
+    func thumbnail(for location: Location) -> UIImage {
+        if location.hasPhoto, let image = location.photoImage {
+            return image.resized(withBounds: CGSize(width: 52, height: 52))
+        }
+        return UIImage()
+    }
 
 }
---
+++ MyLocations/UIImage+Resize.swift
@@ -0,0 +1,26 @@
+//
+//  UIImage+Resize.swift
+//  MyLocations
+//
+//  Created by joe_mac on 09/01/2020.
+//  Copyright © 2020 Joe K. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    func resized(withBounds bounds: CGSize) -> UIImage {
+        let horizontalRatio = bounds.width / size.width
+        let verticalRatio = bounds.height / size.height
+        let ratio = min(horizontalRatio, verticalRatio)
+        let newSize = CGSize(width: size.width * ratio,
+                             height: size.height * ratio)
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, true, 0)
+        draw(in: CGRect(origin: CGPoint.zero, size: newSize))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+    }
+}
